23a28bc6d79a178cea3b8a33928f9e6a315af6b4
umich2020 <jimson@umich.com>
2025-01-26 21:21:27 -0500

    Create basic buttons and set up js function
    0-9 digits created in html and operator created
    Getting the basics of displays. No css done.
    And add js function add, subtract, divide, multiply.
    Work TBD for operator function.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(x,y) {
+    return x+y
+}
+function subtract(x,y) {
+    return x-y
+}
+function multiple(x,y) {
+    return x*y
+}
+function divide(x,y) {
+    return x/y
+}
+function operator(x,y,operator) {
+    return "to be complete"
+}
