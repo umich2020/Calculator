0c149b47dce5d4dc2b03f024b6ec4b408f79236a
umich2020 <jimson@umich.com>
2025-01-27 00:34:22 -0500

    parse first and second value and operation
    able to parse the first and second value (without regex)
    and now will just have to use the function and update it

--- javascript.js
+++ javascript.js
@@ -11,12 +11,29 @@ function multiple(x,y) {
 function divide(x,y) {
     return x/y
 }
-function operator(x,y,operator) {
-    return "to be complete"
-    //could use if statements for operator
+//assume string is 30x60
+function operation(string) {
+    for (let i =0; i < string.length; i++) {
+        if (string[i] === "x" || string[i] === "+" || string[i] === "-" || string[i] === "÷") {
+            thing_todo = string[i]
+            for (let k=i+1; k < string.length; k++) {
+                second = 10 * second + Number(string[k])
+            }
+            break
+        } else {
+            first = 10 * first + Number(string[i])
+        }
+    }
+    console.log(first + " with operator" + thing_todo +"with"+ second)
+    
 }
 //variables
 let display = 0
+let value = "" 
+let first = 0
+let second = 0
+let thing_todo =""
+const opps = ["+", "-", "X", "÷"]
 const html_display = document.getElementById("display")
 //non functions
 //gets each number and output to display
@@ -24,7 +41,27 @@ const numbers = document.querySelectorAll(".number")
 numbers.forEach((number) => {
     number.addEventListener(`click`, () => {
         display = 10 * display + Number(number.textContent)
+        display = display.toString()
+        value += number.textContent
         html_display.textContent = display
+        console.log(value)
+    })
+})
+//gets operators and assign them to variable
+const operators = document.querySelectorAll(".operator")
+operators.forEach((operator) => {
+    operator.addEventListener(`click`, () => {
+        if (opps.some(opp => value.includes(opp))) {
+            console.log(value)
+            operation(value)
+            console.log("operation is successful")
+        }
+        value += operator.textContent
+        display = 0 //i'm not sure if this is right, will fix later
+        html_display.textContent = display
+        console.log(value)
     })
 })
 
+const commence = document.getElementById("commence")
+//commence.addEventListener(`click`, ())
