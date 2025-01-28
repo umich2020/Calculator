f2d60ef7c7d9f74aa3720056df79a3c55c949c21
umich2020 <jimson@umich.com>
2025-01-27 21:05:12 -0500

    Rounding is done
    Used to fixed if the number was a decimal up to 5 places

--- javascript.js
+++ javascript.js
@@ -1,9 +1,20 @@
 //functions
+//coding based function
+function displayCalculator(number) {
+    html_display.textContent = number
+}
+function isNumeric(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  }
+function checkOperators(string) {
+    for (let i=0; i<string.length-2;i++) {
+        if (string[i] === "x" || string[i] === "+" || string[i] === "-" || string[i] === "÷") {
+            return true
+        }
+    }
+    return false
+}
 function add(x,y) {
-    console.log("dipslay is" + display)
-    console.log("x is"+x)
-    console.log("y is"+y)
-
     return x+y
 }
 function subtract(x,y) {
@@ -17,61 +28,83 @@ function divide(x,y) {
 }
 //assume string is 30x60
 function operation(string) {
-    for (let i =0; i < string.length; i++) {
+    for (let i =0; i < string.length - 1; i++) {
         if (string[i] === "=") {
             break
         }
         if (string[i] === "x" || string[i] === "+" || string[i] === "-" || string[i] === "÷") {
-            thing_todo = string[i]
-            for (let k=i+1; k < string.length; k++) {
+            firstOperation = string[i]
+            for (let k=i+1; k < string.length -1; k++) {
                 second = 10 * second + Number(string[k])
+                //console.log("second is "+ second)
             }
             break
         } else {
-            first = 10 * first + Number(string[i])
-        }
+            
+            if(mulitpleOperation === false) {
+                first = 10 * first + Number(string[i])
+            }
+       }
+        
     }
-    switch(thing_todo) {
+    console.log("the second value is "+second)
+    switch(firstOperation) {
         case "+":
             temp_answer = add(Number(first),Number(second))
             displayCalculator(temp_answer)
-            console.log("i thought value was" + value)
-            console.log("but temp_answer is "+temp_answer+'and shall be type'+typeof temp_answer)
-            value = temp_answer
-            console.log(value)
+            first=temp_answer
+            value= temp_answer
+            second=0
+            mulitpleOperation = true
             break
         case "-":
             temp_answer = subtract(Number(first),Number(second))
             displayCalculator(temp_answer)
-            value += temp_answer.toString
+            first=temp_answer
+            value= temp_answer
+            second=0
+            mulitpleOperation = true
             break
         case "x":
             temp_answer = multiply(Number(first),Number(second))
             displayCalculator(temp_answer)
-            value += temp_answer.toString
+            first=temp_answer
+            value= temp_answer
+            second=0
+            mulitpleOperation = true
             break
         case "÷":
             temp_answer = divide(Number(first),Number(second))
             displayCalculator(temp_answer)
-            value += temp_answer.toString
+            first=temp_answer
+            value= temp_answer
+            second=0
+            mulitpleOperation = true
             break
         default:
             console.log("error")
 
     }
+    value += lastOperation
     
 }
-function displayCalculator(number) {
-    html_display.textContent = number
-}
+
 //variables
-let display = 0
-let value = "" 
-let first = 0
-let second = 0
-let temp_answer = 0
-let thing_todo =""//bad naming but this is the operator
-const opps = ["+", "-", "x", "÷"]
+    //display variables
+    let display = 0
+    let value = "" 
+//operation variables
+    let first = 0
+    let second = 0
+    let temp_answer = 0
+    let mulitpleOperation = false
+
+//operation variables
+    let lastOperation =""
+    let firstOperation = ""
+    //let secondOperation = ""
+   //not a variable we just copy and past:  value[value.length -1]    
+const opps = ["+", "-", "x", "÷", "="]
 let html_display = document.getElementById("display")
 //Event listeners
 //gets each number and output to display
@@ -85,26 +118,45 @@ numbers.forEach((number) => {
         console.log(value)
     })
 })
+
 //gets operators and assign them to variable
 const operators = document.querySelectorAll(".operator")
 operators.forEach((operator) => {
     operator.addEventListener(`click`, () => {
-        if (opps.some(opp => value.includes(opp))) {
-            console.log("operation begining")
-            console.log(value)
-            operation(value)
-        }
-        value += operator.textContent
-        display = 0 //i'm not sure if this is right, will fix later
-        html_display.textContent = display
-        if (opps.some(opp => value.includes(opp))) { 
-        displayCalculator(temp_answer)
-        }
         console.log(value)
+        display = 0 //i'm not sure if this is right, will fix later no it's ocrrect
+        html_display.textContent = display
+        if (value[value.length -1] === "x" || value[value.length -1] === "+" || value[value.length -1] === "-" || value[value.length -1] === "÷") {
+            value = value.slice(0,-1)
+        }
+        value+= operator.textContent
+        
+        console.log(value)
+        lastOperation = value[value.length -1]
+        if (isNumeric(value[value.length -2]) && checkOperators(value) === true) {
+            console.log("operation time!")
+            operation(value)
+            console.log('value after operation is ' +value)
+            //console.log('to fixed is' +Number(value[0, value.length-2]).toFixed(5))
+            
+            if (Number(value.slice(0, value.length-1)) % 1 === 0 ) {
+            console.log('whay doesnt this work value is' + Number(value.slice(0, value.length-1)))
+            console.log('value after operation is ' +value)
+                displayCalculator(value)
+            }
+            else {
+                html_display.textContent = Number(value.slice(0, value.length-1)).toFixed(5)
+
+            } 
+            
+        }
+
     })
 })
-
+/*
 const commence = document.getElementById("commence")
 commence.addEventListener(`click`, () => {
     operation(value)
 }) 
+*/
+//set the clear to reset the once operator
